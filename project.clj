2d13ed05fbd8b5acad63649ed706120a250659d6
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-20 12:39:39 -0700

    upped dependencies and increased version
    midje -> 1.5.0
    lein-midje -> 3.0.0
    clojure -> 1.5.0
    tools.logging -> 0.2.6
    clojure-commons -> 1.4.1-SNAPSHOT
    cheshire -> 5.0.2
    clj-http -> 0.6.5
    clj-time -> 0.4.5
    slingshot -> 0.10.3
    url -> 0.0.7

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject esp-common "0.1.0-SNAPSHOT"
+(defproject esp-common "0.1.1-SNAPSHOT"
   :description "Code common to the ESP components."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
-                 [cheshire "5.0.1"]
-                 [clj-http "0.5.3"]
-                 [clj-time "0.4.3"]
-                 [slingshot "0.10.1"]
-                 [com.cemerick/url "0.0.6"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [lein-midje "2.0.0-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
+                 [cheshire "5.0.2"]
+                 [clj-http "0.6.5"]
+                 [clj-time "0.4.5"]
+                 [slingshot "0.10.3"]
+                 [com.cemerick/url "0.0.7"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]
+                                  [lein-midje "3.0.0"]]}}
   :plugins [[lein-ring "0.7.4"]]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
